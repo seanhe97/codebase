ed000923ff84929b286e4f06c6af62d09122543d
sunanhe <sunanhe@tencent.com>
2022-05-05 15:53:22 +0800

    add summary.py

--- utils/summary.py
+++ utils/summary.py
@@ -1,4 +1,5 @@
 """ 
+2022.5.5
 From: https://github.com/rwightman/pytorch-image-models/blob/master/timm/utils/summary.py
 Summary utilities
 Hacked together by / Copyright 2020 Ross Wightman
@@ -12,6 +13,15 @@ except ImportError:
     pass
 
 def get_outdir(path, *paths, inc=False):
+    r"""
+    创建目录，如果目录存在，则自动加 1 
+    参数:
+        path (:obj:`str`):
+            目录路径.
+        do_lower_case (:obj:`bool`, `optional`, defaults to :obj:`True`):
+            Whether or not to lowercase the input when tokenizing.
+
+    """
     outdir = os.path.join(path, *paths)
     if not os.path.exists(outdir):
         os.makedirs(outdir)
@@ -28,6 +38,23 @@ def get_outdir(path, *paths, inc=False):
 
 
 def update_summary(epoch, train_metrics, eval_metrics, filename, write_header=False, log_wandb=False):
+    r"""
+    每个Epoch后打印日志
+    参数:
+        epoch (:obj:`int`):
+            Epoch.
+        train_metrics (:obj:`dict`):
+            训练结果.
+        eval_metrics (:obj:`dict`):
+            测试结果.
+        filename (:obj:`str`):
+            日志文件名.
+        write_header (:obj:`bool`, `optional`, defaults to :obj:`False`):
+            The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
+            token instead.
+        log_wandb (:obj:`bool`, `optional`, defaults to :obj:`False`):
+            是否使用wandb.
+    """
     rowd = OrderedDict(epoch=epoch)
     rowd.update([('train_' + k, v) for k, v in train_metrics.items()])
     rowd.update([('eval_' + k, v) for k, v in eval_metrics.items()])
